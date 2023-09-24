55e18834ffc7e5e71de4a3c7f260f95633116f87
MustaphaBousilDev <112890579+MustaphaBousilDev@users.noreply.github.com>
2023-09-24 17:38:48 +0100

    add room queen

--- src/components/dashboard/Orders.tsx
+++ src/components/dashboard/Orders.tsx
@@ -1,33 +1,40 @@
 import React from 'react'
 import BarChartSimple from '../statistiques/charts/SimpleBarChart'
 import PieChartWithCustomizedLabel from '../statistiques/charts/PieChartWithCustomizedLabel'
+import VerticalCompChart from '../statistiques/charts/VerticalComposedChart'
 
 const Orders = () => {
   return (
     <div className='flex flex-col gap-2'>
-      <div className='flex gap-2 h-60' >
-        <div className='bg-white flex flex-col gap-1 rounded-md shadow-md  w-[49%] h-full'>
+      <div className='flex flex-col xl:flex-row gap-2 min-h-60 xl:h-60' >
+        <div className='bg-white flex flex-col gap-1 rounded-md shadow-md w-full  xl:w-[49.5%] h-full'>
             <BarChartSimple/>
         </div>
-        <div className=' flex gap-2 w-[49%] h-full'>
-            <div className='bg-white w-[49%] rounded-md shadow-md'>
+        <div className=' flex gap-2 w-full xl:w-[49.5%] h-full'>
+            <div className='bg-white w-[49.5%] rounded-md shadow-md'>
               <PieChartWithCustomizedLabel />
             </div>
-            <div className='bg-white w-[49%]  rounded-md shadow-md'>
+            <div className='bg-white w-[49.5%]  rounded-md shadow-md'>
               <PieChartWithCustomizedLabel/>
             </div>
         </div> 
       </div>
-      <div className='bg-white w-[49%] rounded-md flex-col shadow-md p-2 relative flex items-center justify-center'>
-        <h3 className='text-center text-sm font-bold text-gray-700 py-3'>Top Selling Food</h3>
-        <div className=' bg-[#158194] relative  w-80 h-80 rounded-full flex items-center justify-center'>
-          <h3 className='text-white font-bold text-2xl'>1.987$</h3>
-          <div className='absolute top-3 -left-16 z-40 w-44 h-44 bg-primary rounded-full flex items-center justify-center'>
-            <h3 className='text-white font-bold text-2xl'>565$</h3>
+      <div className='flex flex-col xl:flex-row gap-2'>
+        <div className='bg-white w-full xl:w-[49.5%] rounded-md flex-col shadow-md p-2 py-7 relative flex items-center justify-center'>
+          <h3 className='text-center text-sm font-bold text-gray-700 py-3'>Top Selling Food</h3>
+          <div className=' bg-[#158194] relative w-56 h-56  lg:w-80 lg:h-80 rounded-full flex items-center justify-center'>
+            <h3 className='text-white font-bold text-2xl'>1.987$</h3>
+            <div className='absolute top-3 -left-16 z-40 w-24 h-24 lg:w-44 lg:h-44 bg-primary rounded-full flex items-center justify-center'>
+              <h3 className='text-white font-bold text-2xl'>565$</h3>
+            </div>
+            <div className='absolute -bottom-0  -left-16 z-40 w-28 h-28 lg:w-32 lg:h-32 bg-[#92d9e6] rounded-full flex items-center justify-center'>
+              <h3 className='text-white font-bold text-xl'>565$</h3>
+            </div>
           </div>
-          <div className='absolute -bottom-0  -left-16 z-40 w-32 h-32 bg-[#92d9e6] rounded-full flex items-center justify-center'>
-            <h3 className='text-white font-bold text-xl'>565$</h3>
-          </div>
+        </div>
+        <div className='bg-white w-full xl:w-[49.5%] rounded-md shadow-md p-2 py-7  flex flex-col '>
+          <h3 className='text-center font-bold text-xl'>Top Something</h3>
+          <VerticalCompChart/>
         </div>
       </div>
     </div>
--- src/components/dashboard/Rooms.tsx
+++ src/components/dashboard/Rooms.tsx
@@ -1,8 +1,81 @@
 import React from 'react'
+import Image from 'next/image'
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+import RedeemIcon from '@mui/icons-material/Redeem';
+import WifiIcon from '@mui/icons-material/Wifi';
+import CoffeeIcon from '@mui/icons-material/Coffee';
+import NoMealsIcon from '@mui/icons-material/NoMeals';
+import LocalBarIcon from '@mui/icons-material/LocalBar';
 
 const Rooms = () => {
   return (
-    <div>Rooms</div>
+    <div className='flex flex-col gap-2 '>
+      <div className='rounded-md flex flex-wrap items-center gap-2 xl:gap-3   p-1'>
+          <div className='w-full lg:w-[49%] flex items-center justify-center flex-col xl:w-[24%] bg-primaryLight p-4 rounded-md shadow-md'>
+            one
+          </div>
+          <div className='w-full lg:w-[49%] flex items-center justify-center flex-col xl:w-[24%] bg-primaryLight p-4 rounded-md shadow-md'>
+            one
+          </div>
+          <div className='w-full lg:w-[49%] flex items-center justify-center flex-col xl:w-[24%] bg-primaryLight p-4 rounded-md shadow-md'>
+            one
+          </div>
+          <div className='w-full lg:w-[49%] flex items-center justify-center flex-col xl:w-[24%] bg-primaryLight p-4 rounded-md shadow-md'>
+            one
+          </div>
+      </div>
+      <div className='queen__room flex h-72  bg-white border rounded-sm w-[70%]  '>
+        <div className=' bg-center bg-cover  w-[42%] bg-[url("https://goodhomes.wwmindia.com/content/2020/dec/ali-bedroom-table-with-rooshad-shroff1607602520.jpg")]'>
+          
+        </div>
+        <div className='p-3 w-[58%] flex flex-col gap-2'>
+          <div className='flex justify-between items-center w-full ' >
+            <h3 className='font-bold text-gray-800'>Luxiry Queen Room</h3>
+            <div className='flex flex-col gap-1'>
+              <h3 className='flex gap-1 items-center'>
+                <span className=' text-gray-400 text-xs'>$208.99</span>
+                <span className='text-md text-primary font-bold'>$109.65</span>
+              </h3>
+            </div>
+          </div>
+          <div className=' flex gap-2  items-center  justify-between'>
+            <div className='flex gap-1'>
+              <div className=' flex bg-greenLight items-center gap-1 rounded-md p-[6px] px-2'>
+                <LocalOfferIcon style={{fontSize:13,color: 'rgb(10 212 37)'}}
+                />
+                <span className='text-xs font-semibold text-greenText'>Special Rate</span>
+              </div>
+              <div className='flex bg-yellow-100 items-center gap-1 rounded-md p-[6px] px-2'>
+                <RedeemIcon style={{fontSize:13,color: 'rgb(202 138 4)'}}/>
+                <span className='text-xs font-semibold text-yellow-600'>Loyelty Members</span>
+              </div>
+            </div>
+            <div>
+              <p className=' text-xs font-semibold text-gray-400'>1 night, 2 persone</p>
+            </div>
+          </div>
+          <div className=' my-2'>
+            <p className='text-sm text-gray-700'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus similique quisquam 
+              sed non repellat voluptate sed non repellat voluptate sed non repellat voluptate sed non repellat voluptate 
+              
+            </p>
+          </div>
+          <div>
+            <ul className='flex gap-4 items-center'>
+              <li><WifiIcon className='text-primary transition cursor-pointer' style={{fontSize:20}} /></li>
+              <li><CoffeeIcon className='hover:text-primary transition cursor-pointer' style={{fontSize:20}} /></li>
+              <li><NoMealsIcon className='hover:text-primary transition cursor-pointer' style={{fontSize:20}} /></li>
+              <li><LocalBarIcon className='hover:text-primary transition cursor-pointer' style={{fontSize:20}} /></li>
+            </ul>
+          </div>
+          <div className= ' my-5'>
+            <button className='bg-primary rounded-md shadow-md text-sm active:scale-95 transition text-white font-semibold flex items-center justify-center px-4 py-[6px]'>
+              Reservée
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
